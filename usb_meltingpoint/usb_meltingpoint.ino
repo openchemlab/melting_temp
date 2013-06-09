28116d31b95fcc9622b08fecb0a723e943571f13
chaac <Jack.robison@gmail.com>
2013-06-09 00:22:14 -0300

    Update usb_meltingpoint.ino

--- usb_meltingpoint/usb_meltingpoint.ino
+++ usb_meltingpoint/usb_meltingpoint.ino
@@ -1,12 +1,23 @@
 /*
+*********************************************************************
+*********************************************************************
 Melting point apparatus for the OpenChemLab project by chaac
 
-Visit openchemlab.org , check out the forums.
+To use this program: 
+1. build the circuit
+2. upload this to an arduino
+3. plug in power supply
+4. open serial dialog in arduino
+5. type target temp and watch the temp rise
 
+Visit openchemlab.org , check out the forums.
 Donations welcome
 
 Bitcoin: 18mG29fRKU4NdhPRfP98nMV4fXMJ73gbjn
 Litecoin: Lb2ABK6Zy5dnza14FaKDTNQsvrXTMXoEgL
+
+*********************************************************************
+*********************************************************************
 */
 
 #include <PID_v1.h>
